57f11640ed65b37d3859fc4525a4bb433e50c9e7
byronaero <byronaero@gmail.com>
2017-11-14 18:30:19 +1100

    Update site-Veuron.js

--- js/site-Veuron.js
+++ js/site-Veuron.js
@@ -79,7 +79,7 @@ function viewdata(){
                 },
                 "overlay": {
                     "type": "image",
-                    "url": "https://crossorigin.me/https://github.com/byronaero/test/tree/gh-pages/img/caspian.jpg",
+                    "url": "https://github.com/byronaero/test/blob/gh-pages/js/caspian.jpg",
                     "coordinates": [
                         [153.007059, -27.553928], //top left
                         [153.007558, -27.553928],
