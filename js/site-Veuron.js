cc5b5c28950a2df99d2f095f23ef7205c1c83528
byronaero <byronaero@gmail.com>
2017-11-14 18:32:25 +1100

    Update site-Veuron.js

--- js/site-Veuron.js
+++ js/site-Veuron.js
@@ -79,7 +79,7 @@ function viewdata(){
                 },
                 "overlay": {
                     "type": "image",
-                    "url": "https://github.com/byronaero/test/blob/gh-pages/js/caspian.jpg",
+                    "url": "http://nearsat.com/img/caspian.jpg",
                     "coordinates": [
                         [153.007059, -27.553928], //top left
                         [153.007558, -27.553928],
